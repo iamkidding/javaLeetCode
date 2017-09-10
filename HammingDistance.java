e6f4af2e3e7c7cb0cb9b094d4785a2b9fa15da47
Song <iamkindding@163.com>
2017-09-11 00:29:03 +0800

    TwoSum未完成，HammingDistance排在60%，测试18s‘

---
+++ HammingDistance.java
@@ -0,0 +1,30 @@
+package javaLeetCode;
+//ÅÅÃû60%, ²âÊÔ18ms
+public class HammingDistance {
+    public static int hammingDistance(int x, int y) {
+        int i = 0;
+        int xl = 0, yl = 0;
+        while(x > 0 && y > 0){
+        	xl = x % 2;
+        	x = x / 2;
+        	yl = y % 2;
+        	y = y / 2;
+        	if (xl != yl) i++;
+        }
+        while(x != 0){
+       	    xl = x % 2;
+       	    x = x / 2;
+       	    if (xl == 1) i++;
+        }
+       	while(y != 0){
+       	    yl = y % 2;
+       	    if (yl == 1) i++;       	  
+       	    y = y / 2;
+        }
+        return i;
+    }
+	public static void main(String[] args) {
+        int m = hammingDistance(4, 11);
+        System.out.println(m);
+	}
+}
---
+++ TwoSum.java
@@ -0,0 +1,74 @@
+package javaLeetCode;
+/**
+* @author j
+*
+*/
+public class TwoSum {
+
+	/**
+	 * @param args
+	 */
+	private static void exch(int[] a, int i, int j){
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	public static void quickSort(int[] a){
+		quickSort(a, 0, a.length-1);
+	}
+	private static void quickSort(int[] a, int lo, int hi){
+		if (hi <= lo) return;
+		int j = partition(a, lo, hi);
+		quickSort(a, lo, j-1);
+		quickSort(a, j+1, hi);
+	}
+	private static int partition(int[] a, int lo, int hi){
+		int v =  a[lo];
+		int i = lo, j = hi;
+		while (true){
+			while (a[++i] < v) if( i == hi) break;
+			while (a[j--] > v) if( j == lo) break;
+			if(i >= j) break;
+			exch(a, i, j);
+			}
+		exch(a, lo, j);
+		return j;
+	}
+	public static void findTwoSum(int[] a, int target){
+		int j = -1;
+		quickSort(a);
+		int[] result = new int[2];
+		for (int i=0; i < a.length; i++){
+			int t = target - a[i];
+			if (t < a[i])
+				j = binarySearch(a, 0, i-1, t);
+			else if(t > a[i])
+			    j = binarySearch(a, i+1, a.length-1, t);
+			if (j != -1){
+				result[0] = i;
+			    result[1] = j;
+			    break;
+			}
+		}
+		for (int i=0; i < result.length; i++)
+	        System.out.print(result[i]);
+	}
+	private static int binarySearch(int[] a, int lo, int hi, int key){
+		while(lo <= hi){
+			int mid = lo + (hi - lo)/2;
+			if (a[mid] < key)
+				hi = mid - 1;
+			else if (a[mid] > key)
+				lo = mid + 1;
+			else if (a[mid] == key)
+				return mid;
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] nums = {14, 21, 2, 7, 11, 15};
+       findTwoSum(nums, 9);
+	}
+
+}
