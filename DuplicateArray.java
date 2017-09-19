a5365f9788f45232f65f9781cbe70dd3efd6adda
Song <iamkindding@163.com>
2017-09-19 23:35:17 +0800

    看过剑指offer书上的答案解决了测试数组为空指针的问题，即输入为{}

--- DuplicateArray.java
+++ DuplicateArray.java
@@ -11,27 +11,26 @@ public class DuplicateArray {
     //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
     // Return value:       true if the input is valid, and there are some duplications in the array number
     //                     otherwise false
-	public static boolean duplicate(int numbers[],int length,int [] duplication) {
-		if(numbers.length ==  0) return false;
-	    quickSort(numbers);
-	    for(int i = 0; i < length - 1; i++)
-	    	if(numbers[i] == numbers[i+1]) {
-	    		duplication[0] = numbers[i];
-	    		return true;
-	    	}
+	public static boolean duplicate(int numbers[], int length, int [] duplication) {
+		if(numbers == null || numbers.length <= 0) return false;
+	        quickSort(numbers);
+	        for(int i = 0; i < length - 1; i++)
+	    	    if(numbers[i] == numbers[i+1]) {
+	    		    duplication[0] = numbers[i];
+	    		    return true;
+	    	    }
 	    return false;
     }
-
 	private static void quickSort(int[] nums){
 		quickSort(nums, 0, nums.length-1);
 	}
 	private static void quickSort(int[] a, int lo, int hi){
-		if (lo <= hi) return;
+		if (hi <= lo) return;
 		int lt=lo, i=lo+1, gt=hi;
 		int v = a[lo];
 		while(i <= gt){
 			if(v > a[i]) exch(a, lt++, i++);
-			else if(v < a[i]) exch(a, i, --gt);
+			else if(v < a[i]) exch(a, i, gt--);
 			else i++;
 		}
 		quickSort(a, lo, lt-1);
@@ -44,8 +43,11 @@ public class DuplicateArray {
 	}
     public static void main(String[] args){
     	int[] nums = {2, 1, 3, 1, 4};
-    	quickSort(nums);
+    	quickSort(nums, 0, nums.length-1);
     	for(int i=0; i<nums.length; i++)
     		System.out.print(nums[i]);
+    	int[] num = {};
+    	System.out.println(num==null);
+    	System.out.println(num.length);
     }
 }
