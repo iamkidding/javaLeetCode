0052df30e1afe1961b8e39e1055cf2fe78e77938
Song <iamkindding@163.com>
2017-09-22 22:15:56 +0800

    厉害了，看书上的解题思路后做出来了。精妙，佩服

--- TwoDArrayFind.java
+++ TwoDArrayFind.java
@@ -5,31 +5,14 @@ public class TwoDArrayFind {
 //	从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 	public static boolean Find(int target, int [][] array) {
         if(array == null || array.length <= 0) return false;
-        int i = rank(target, array) - 1;
-        if (array[i][0] == target) return true;
-        else if (binarySearch(target, array[i]))
-        	return true;
-        return false;
-    }
-	private static boolean binarySearch(int target, int[] array){
-		int lo=0, hi=array.length-1;
-		while(lo <= hi){
-			int mid = (hi - lo) / 2 + lo;
-			if (array[mid] > target) hi = mid - 1;
-			else if (array[mid] < target) lo = mid + 1;
-			else return true;
-		}
-		return false;
-	}
-	private static int rank(int target, int [][] array){
-		int lo=0, hi=array.length-1;
-		while (lo <= hi){
-			int mid = (hi - lo) / 2 + lo;
-			if (array[mid][0] > target) hi = mid - 1;
-			else if (array[mid][0] < target) lo = mid + 1;
-			else return mid;
-		}
-		return lo;
+        int i = 0, j = array[0].length-1;
+        while (j >= 0 && i < array[0].length){
+            int cmp = array[i][j];
+            if (target == cmp) return true;
+            else if (target < cmp) j--;
+            else i++;
+        }
+        return false;        
 	}
 	
 	public static void main(String[] args) {
@@ -37,5 +20,4 @@ public class TwoDArrayFind {
         int [][] array = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
         System.out.println(Find(7, array));
 	}
-
 }
