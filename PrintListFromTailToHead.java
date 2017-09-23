5db9abf6919bad5d060cef7d8a8f3db685ce7bdb
Song <iamkindding@163.com>
2017-09-23 13:31:57 +0800

    使用了一个数组逆序存储链表的值。书上的思路使用了栈，但Java没有栈，还得自己写。以我目前水平，这样就不知道哪个解法更好了

---
+++ PrintListFromTailToHead.java
@@ -0,0 +1,23 @@
+package javaLeetCode;
+
+import java.util.ArrayList;
+public class PrintListFromTailToHead {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+                
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
