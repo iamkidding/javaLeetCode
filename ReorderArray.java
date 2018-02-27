130d798c4421a374c1f1c63b9c73f31ee67ad476
iamkidding <iamkindding@163.com>
2018-02-27 14:18:11 +0800

    书上的题和网站题不一样。使用了ArrayList，也可以用一个等长数组代替。用空间换时间。

--- ReorderArray.java
+++ ReorderArray.java
@@ -1,5 +1,7 @@
 package javaLeetCode;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 public class ReorderArray {
     public static int[] reorderArray(int[] array){
         int indexEven = array.length - 1;
@@ -16,6 +18,23 @@ public class ReorderArray {
         }
         return array;
     }
+
+    public void reorderArrayC(int[] array){
+        if (array == null ||array.length == 0) return;
+
+        ArrayList<Integer> aux = new ArrayList<>();
+        for (int i=0; i<array.length; i++){
+            if ((array[i] & 0x1) == 1) aux.add(array[i]);
+        }
+        for (int i=0; i<array.length; i++){
+            if ((array[i] & 0x1) == 0) aux.add(array[i]);
+        }
+        for (int i=0; i<array.length; i++){
+            array[i] = aux.get(i);
+        }
+    }
+
+
     public static void main(String[] args){
 //        System.out.println(12 & 0x1);
         int[] array = {1, 4, 6, 5, 7, 9, 10, 12};
