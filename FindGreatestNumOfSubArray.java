661213d6a87b7f70814151ad00ace6b7fb8134ce
iamkidding <iamkindding@163.com>
2018-03-09 15:58:40 +0800

    自己没有思路

--- FindGreatestNumOfSubArray.java
+++ FindGreatestNumOfSubArray.java
@@ -1,15 +1,24 @@
 package javaLeetCode;
 
 public class FindGreatestNumOfSubArray {
-    public int findGreatestNumOfSubArray(int[] array){
+    public int findGreatestNumOfSubArrayB(int[] array){
         if (array.length == 0) return 0;
-        int sum=0, cmp=0;
 
-        for(int i=0; i<array.length; i++){
-            cmp += array[i];
+        int sum=0x80000000, cmp=0;
+
+        for (int i=0; i<array.length; i++){
+            if (cmp <= 0) cmp = array[i];
+            else cmp += array[i];
+
             if (cmp > sum) sum = cmp;
         }
 
         return sum;
+    }
+
+    public static void main(String[] args){
+        int[] test = {-2,-8,-1,-5,-9};
+
+        System.out.print((new FindGreatestNumOfSubArray()).findGreatestNumOfSubArrayB(test));
     }
 }
