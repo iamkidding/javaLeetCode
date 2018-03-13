90fd2b78423fa0578c4633568249f8136c26a52a
iamkidding <iamkindding@163.com>
2018-03-13 20:40:24 +0800

    书上解法使用了库函数。网上解法也使用了许多库函数。暂时解不出。

---
+++ PrintMinNumber.java
@@ -0,0 +1,20 @@
+package javaLeetCode;
+
+public class PrintMinNumber {
+    public String printMinNumber(int[] numbers){
+        if (numbers.length == 0) return null;
+
+        String[] intConvert = new String[numbers.length];
+
+        for(int i=0; i<numbers.length; i++){
+            intConvert[i] = "" + numbers[i];
+        }
+
+
+        return null;
+    }
+
+    public static void main(String[] args){
+        System.out.print("1".charAt(0));
+    }
+}
