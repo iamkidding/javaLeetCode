0e0b74f60da56d87038688fe04a609809ba82101
iamkidding <iamkindding@163.com>
2018-05-15 20:26:04 +0800

    网站上与书上题目不同，在线编程需要将二叉树按行打印，所以需要记录每层的节点数

--- PrintTreeByRow.java
+++ PrintTreeByRow.java
@@ -13,14 +13,49 @@ public class PrintTreeByRow {
         Queue<TreeNode> temp = new LinkedList<>();
 
         temp.add(pRoot);
+        int start = 0;
+        int endCurr = 1, endNext = 0; // endCurr记录当前层有几个节点，endNext记录下一层有几个节点
+        TreeNode node;
         while(!temp.isEmpty()){
-            TreeNode node = temp.poll();
             ArrayList<Integer> row = new ArrayList<>();
-            row.add(node.val);
-            if (node.left != null)
-                temp.add(node.left);
-            if (node.right != null)
-                temp.add(node.right);
+            while (start < endCurr){
+                node = temp.poll();
+                row.add(node.val);
+                if (node.left != null){
+                    temp.add(node.left);
+                    endNext++;
+                }
+                if (node.right != null){
+                    temp.add(node.right);
+                    endNext++;
+                }
+                start++;
+            }
+            print.add(row);
+            start = 0;
+            endCurr = endNext; // 在一个循环进入下一层，节点数变为为下一层
+            endNext = 0;  // 下一层节点数重新计算
         }
+
+        return print;
+    }
+
+    public static void main(String[] args){
+        TreeNode n1 = new TreeNode(8);
+        TreeNode n2 = new TreeNode(6);
+        TreeNode n3 = new TreeNode(10);
+        TreeNode n4 = new TreeNode(5);
+        TreeNode n5 = new TreeNode(7);
+        TreeNode n6 = new TreeNode(9);
+        TreeNode n7 = new TreeNode(11);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+
+        ArrayList<ArrayList<Integer>> p = new PrintTreeByRow().print(n1);
+        System.out.print(p);
     }
 }
