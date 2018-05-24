cbeb3d9d368fe53c766ae57e560e0a2147246300
iamkidding <iamkindding@163.com>
2018-05-24 20:41:51 +0800

    思路不对

--- CheckPossibillity.java
+++ CheckPossibillity.java
@@ -9,29 +9,18 @@ public class CheckPossibillity {
         int v = nums[N/2];
         int i=0, j=N-1;
 
-        while (true){
-            while (nums[i++] != v) {if (nums[i] > v) {count++;System.out.println("in");}}
-            while (nums[j--] != v) {if (nums[j] < v) count++;}
+        while (i <= j) {
+            if (nums[i] > v) count++;
+            i++;
+            if (nums[j] < v) count++;
+            j--;
             if (count >= 2) return false;
-            System.out.println(i);
-            System.out.println("out");
-            if (j <= i) break;
-
-//  exch(nums, i, j);
         }
-
-
         return true;
     }
 
-    private void exch(int[] a, int i, int j){
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-
     public static void main(String[] args){
-        int[] a = {4, 2, 1};
+        int[] a = {1, 1, 1};
         System.out.println(new CheckPossibillity().checkPossibillty(a));
     }
 }
